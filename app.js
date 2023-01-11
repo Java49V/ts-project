4184f3534e95a314baf0802e0a1a3ea096a0506b
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-01-11 14:44:08 +0300

    HW #31 oop-ts

--- app.js
+++ app.js
@@ -1,89 +1,23 @@
 "use strict";
-const aCodeAscii = 'a'.charCodeAt(0);
-const zCodeAscii = 'z'.charCodeAt(0);
-const nEnglishLetters = zCodeAscii - aCodeAscii + 1;
-function shiftCipher(str, shift = 1) {
-    return cipherDecipher(str, shift, mapperCipher);
-}
-function shiftDecipher(str, shift = 1) {
-    return cipherDecipher(str, shift, mapperDecipher);
-}
-function cipherDecipher(str, shift, mapperFun) {
-    //const arStr: string[] = Array.from(str);
-    const arStr = Array.from(str);
-    const arRes = arStr.map(symb => {
-        let res = symb;
-        if (symb <= 'z' && symb >= 'a') {
-            res = mapperFun(symb, shift);
-        }
-        return res;
-    });
-    return arRes.join('');
-}
-function mapperCipher(symb, shift) {
-    const actualShift = (symb.charCodeAt(0) - aCodeAscii + shift) % nEnglishLetters;
-    return String.fromCharCode(aCodeAscii + actualShift);
-}
-function mapperDecipher(symb, shift) {
-    const actualShift = (zCodeAscii - symb.charCodeAt(0) + shift) % nEnglishLetters;
-    return String.fromCharCode(zCodeAscii - actualShift);
-}
-function testCipherDecipher(data, testName) {
-    console.log(`${"*".repeat(10)}${testName}${"*".repeat(10)}`);
-    const funForTest = testName === "cipherTest" ? shiftCipher : shiftDecipher;
-    data.forEach((obj => console.log(`str=${obj.str}, shift=${obj.shift || 1} => ${funForTest(obj.str, obj.shift)}`)));
-}
-const dataForCipherTest = [
-    { str: "abc" }, { str: "abz", shift: 1000 }
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rectangle_1 = require("./Rectangle");
+const Square_1 = require("./Square");
+const ShapesContainer_1 = require("./ShapesContainer");
+const Cipher_1_1 = require("./Cipher.1");
+const Decipher_1_1 = require("./Decipher.1");
+const inputStr = 'az2:W';
+const choiceC = new Cipher_1_1.Cipher('a'.charCodeAt(0), 'z'.charCodeAt(0), 50);
+const strS = choiceC.cipher(inputStr);
+console.log(inputStr, strS, choiceC.decipher(strS));
+const choiceD = new Decipher_1_1.Decipher('a'.charCodeAt(0), 'z'.charCodeAt(0), 50);
+const strA = choiceD.cipher(inputStr);
+console.log(inputStr, strA, choiceD.decipher(strA));
+const shapes = [
+    new Rectangle_1.Rectangle(5, 7),
+    new Square_1.Square(6)
 ];
-testCipherDecipher(dataForCipherTest, "cipherTest");
-const dataForDecipherTest = [
-    { str: "bcd" }, { str: "mnl", shift: 1000 }
-];
-testCipherDecipher(dataForDecipherTest, "decipherTest");
-// console.log(shiftCipher("abz", 1000))
-// function shiftCipher(str: string, shift: number = 1): string {
-//   let arrIn: Array<string>;
-//   let arrOut: Array<string>;
-//   let res: string;
-//   arrIn = Array.from(str);
-//   arrOut = arrIn.map(function (everySymb: string) {
-//     let code: number = everySymb.charCodeAt(0);
-//     if (code >= 97 && code <= 122) {
-//       for (let i = 0; i < shift; i++) {
-//         code == 122 ? (code = 97) : code++;
-//         res = String.fromCharCode(code);
-//       }
-//     } else {
-//       res = everySymb;
-//     }
-//     return res;
-//   });
-//   return arrOut.join("");
-// }
-// console.log(`'abc' = ${shiftCipher("abc")}`);
-// console.log(`'abz', shift 27 = ${shiftCipher("abz", 27)}`);
-// console.log(`'abz', shift 1000 = ${shiftCipher("abz", 1000)}`);
-// function shiftDecipher(str: string, shift: number = 1): string {
-//   let arrIn: Array<string>;
-//   let arrOut: Array<string>;
-//   let res: string;
-//   arrIn = Array.from(str);
-//   arrOut = arrIn.map(function (everySymb: string) {
-//     let code: number = everySymb.charCodeAt(0);
-//     if (code >= 97 && code <= 122) {
-//       for (let i = 0; i < shift; i++) {
-//         code == 97 ? (code = 122) : code--;
-//         res = String.fromCharCode(code);
-//       }
-//     } else {
-//       res = everySymb;
-//     }
-//     return res;
-//   });
-//   return arrOut.join("");
-// }
-// console.log(`'bcd': ${shiftDecipher("bcd")}`);
-// console.log(`'bca', shift 27: ${shiftDecipher("bca", 27)}`);
-// console.log(`'mnl', shift 1000: ${shiftDecipher("mnl", 1000)}`);
+const container = new ShapesContainer_1.ShapesContainer(shapes);
+const sumOfShapes = container.getSquare();
+const sumOfPerim = container.getPerimeter();
+console.log(sumOfShapes, sumOfPerim);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,105 +1,26 @@
-const aCodeAscii: number = 'a'.charCodeAt(0);
-const zCodeAscii: number = 'z'.charCodeAt(0);
-const nEnglishLetters: number = zCodeAscii - aCodeAscii + 1;
- 
-function shiftCipher(str: string, shift: number = 1): string {
-    return cipherDecipher(str, shift, mapperCipher);
-}
-function shiftDecipher(str: string, shift: number = 1) : string {
-    return cipherDecipher(str, shift, mapperDecipher);
-}
-type MapperFunction = (symb: string, shift: number) => string;
-function cipherDecipher(str: string, shift: number,
-     mapperFun: MapperFunction): string {
-        //const arStr: string[] = Array.from(str);
-        const arStr: Array<string> = Array.from(str);
-        const arRes: Array<string> = arStr.map(symb => {
-            let res: string = symb;
-            if (symb <= 'z' && symb >= 'a') {
-                res = mapperFun(symb, shift);
-            }
-            return res;
-        })
-        return arRes.join('');
-     }
-     function mapperCipher(symb: string, shift: number) : string {
-        const actualShift: number = (symb.charCodeAt(0) - aCodeAscii + shift) % nEnglishLetters;
-        return String.fromCharCode(aCodeAscii + actualShift);
-     }
-     function mapperDecipher(symb: string, shift: number): string {
-        const actualShift: number = (zCodeAscii - symb.charCodeAt(0) + shift) % nEnglishLetters;
-        return String.fromCharCode(zCodeAscii - actualShift);
-     }
-     type TestObj = {
-        str: string,
-         shift?: number
-     }
-     function testCipherDecipher(data: Array<TestObj>,
-         testName: string): void{
-            console.log(`${"*".repeat(10)}${testName}${"*".repeat(10)}`)
-            const funForTest: MapperFunction
-             = testName === "cipherTest" ? shiftCipher : shiftDecipher;
-data.forEach((obj => console.log(`str=${obj.str}, shift=${obj.shift || 1} => ${funForTest(obj.str, obj.shift)}`))) 
-         }
-         const dataForCipherTest: Array<TestObj> = [
-            {str: "abc"}, {str: "abz", shift: 1000}
-         ];
-         testCipherDecipher(dataForCipherTest, "cipherTest");
-         const dataForDecipherTest: Array<TestObj> = [
-            {str: "bcd"}, {str: "mnl", shift: 1000}
-         ];
-         testCipherDecipher(dataForDecipherTest, "decipherTest");
+import { Shape } from "./ShapeInterface"
+import { Rectangle } from "./Rectangle"
+import { Square } from "./Square"
+import { ShapesContainer } from "./ShapesContainer"
+import { Cipher } from "./Cipher.1"
+import { Decipher } from "./Decipher.1"
 
-// console.log(shiftCipher("abz", 1000))
+const inputStr = 'az2:W';
+const choiceC = new Cipher('a'.charCodeAt(0), 'z'.charCodeAt(0), 50);
+const strS: string = choiceC.cipher(inputStr);
+console.log(inputStr, strS, choiceC.decipher(strS));
+const choiceD = new Decipher('a'.charCodeAt(0), 'z'.charCodeAt(0), 50);
+const strA: string = choiceD.cipher(inputStr);
+console.log(inputStr, strA, choiceD.decipher(strA));
 
-// function shiftCipher(str: string, shift: number = 1): string {
-//   let arrIn: Array<string>;
-//   let arrOut: Array<string>;
+const shapes: Array<Shape> = [
+    new Rectangle(5, 7),
+    new Square(6)
+];
 
-//   let res: string;
-//   arrIn = Array.from(str);
-//   arrOut = arrIn.map(function (everySymb: string) {
-//     let code: number = everySymb.charCodeAt(0);
+const container = new ShapesContainer(shapes);
 
-//     if (code >= 97 && code <= 122) {
-//       for (let i = 0; i < shift; i++) {
-//         code == 122 ? (code = 97) : code++;
-//         res = String.fromCharCode(code);
-//       }
-//     } else {
-//       res = everySymb;
-//     }
-//     return res;
-//   });
-//   return arrOut.join("");
-// }
-// console.log(`'abc' = ${shiftCipher("abc")}`);
-// console.log(`'abz', shift 27 = ${shiftCipher("abz", 27)}`);
-// console.log(`'abz', shift 1000 = ${shiftCipher("abz", 1000)}`);
+const sumOfShapes: number = container.getSquare();
+const sumOfPerim: number = container.getPerimeter();
+console.log(sumOfShapes, sumOfPerim);
 
-// function shiftDecipher(str: string, shift: number = 1): string {
-//   let arrIn: Array<string>;
-//   let arrOut: Array<string>;
-
-//   let res: string;
-//   arrIn = Array.from(str);
-//   arrOut = arrIn.map(function (everySymb: string) {
-//     let code: number = everySymb.charCodeAt(0);
-
-//     if (code >= 97 && code <= 122) {
-//       for (let i = 0; i < shift; i++) {
-//         code == 97 ? (code = 122) : code--;
-//         res = String.fromCharCode(code);
-//       }
-//     } else {
-//       res = everySymb;
-//     }
-//     return res;
-//   });
-
-//   return arrOut.join("");
-// }
-
-// console.log(`'bcd': ${shiftDecipher("bcd")}`);
-// console.log(`'bca', shift 27: ${shiftDecipher("bca", 27)}`);
-// console.log(`'mnl', shift 1000: ${shiftDecipher("mnl", 1000)}`);
